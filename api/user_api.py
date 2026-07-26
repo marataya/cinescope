14a3bff6770fdc56b665b69ce5f3868852202df6
Marat Ayaibergenov <marataya@gmail.com>
2026-07-26 12:39:53 +0500

    refactor: user.py, user_creds.py, user_api.py

--- api/user_api.py
+++ api/user_api.py
@@ -1,13 +1,35 @@
-from constants import AUTH_BASE_URL, Endpoints
+from constants.endpoints import Endpoints
+from constants.urls import AUTH_BASE_URL
 from custom_requester.custom_requester import CustomRequester
 
-
-class UserAPI(CustomRequester):
+class UserApi(CustomRequester):
     def __init__(self, session):
         super().__init__(session=session, base_url=AUTH_BASE_URL)
 
-    def get_users(self, expected_status=200, **kwargs):
-        return self.send_request("GET", Endpoints.USERS, expected_status=expected_status, **kwargs)
+    def create_user(self, user_data, expected_status=201):
+        return self.send_request(
+            "POST",
+            Endpoints.USERS,
+            data=user_data,
+            expected_status=expected_status
+        ).json()
 
-    def get_user(self, user_id, expected_status=200, **kwargs):
-        return self.send_request("GET", Endpoints.USER_BY_ID.format(user_id), expected_status=expected_status, **kwargs)
+    def get_user(self, user_id, expected_status=200):
+        return self.send_request(
+            "GET",
+            Endpoints.USER_BY_ID.format(user_id),
+            expected_status=expected_status
+        ).json()
+
+    def delete_user(self, user_id, expected_status=200):
+        # DELETE возвращает пустой body, нельзя .json()
+        response = self.send_request(
+            "DELETE",
+            Endpoints.USER_BY_ID.format(user_id),
+            expected_status=expected_status
+        )
+        # если тело пустое - возвращаем {}
+        try:
+            return response.json()
+        except:
+            return {}
